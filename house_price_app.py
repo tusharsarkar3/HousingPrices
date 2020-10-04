b0f02abf2747fac12ca135d6a60bc9e3990f3572
tusharsarkar3 <59759357+tusharsarkar3@users.noreply.github.com>
2020-10-05 01:32:01 +0530

    Update house_price_app.py

--- house_price_app.py
+++ house_price_app.py
@@ -267,10 +267,10 @@ def main():
     if st.sidebar.checkbox("Show raw data", False):
         st.subheader("USA House Prices")
         st.write(df)
-        st.markdown(
-            "This [data set](https://archive.ics.uci.edu/ml/datasets/Mushroom) includes descriptions of hypothetical samples corresponding to 23 species of gilled mushrooms "
-            "in the Agaricus and Lepiota Family (pp. 500-525). Each species is identified as definitely edible, definitely poisonous, "
-            "or of unknown edibility and not recommended. This latter class was combined with the poisonous one.")
+#         st.markdown(
+#             "This [data set](https://archive.ics.uci.edu/ml/datasets/Mushroom) includes descriptions of hypothetical samples corresponding to 23 species of gilled mushrooms "
+#             "in the Agaricus and Lepiota Family (pp. 500-525). Each species is identified as definitely edible, definitely poisonous, "
+#             "or of unknown edibility and not recommended. This latter class was combined with the poisonous one.")
 
 
 if __name__ == '__main__':
